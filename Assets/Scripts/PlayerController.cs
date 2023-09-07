24267e9926b9a853c489848868fa54e04b053aaa
SHARAN MURLI <murli@usc.edu>
2023-09-06 18:35:12 -0700

    Car Prototype
    Junior Programmer Pathway: Your car should be drivable, capable of turning, and have a camera following from behind.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float speed=5.0f;
+        public float turnSpeed;
+        public float horizontalInput;
+        public float forwardInput;
+
+
+    // Update is called once per frame
+    void Update()
+    {   
+        horizontalInput=Input.GetAxis("Horizontal");
+        forwardInput=Input.GetAxis("Vertical");
+        //Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up,  turnSpeed  * horizontalInput * Time.deltaTime);
+        
+    }
+}
